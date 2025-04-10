234c3e24a840fd5cb433badd164e1a7e356c65e6
d1mznpc <dimasakuq@gmail.com>
2025-04-10 19:04:00 +0700

    membuat fitur scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
 import { BrowserRouter } from "react-router"; 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './dist/css/main.css'
@@ -8,6 +9,7 @@ import './dist/css/main.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
